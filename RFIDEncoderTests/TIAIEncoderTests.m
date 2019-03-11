a7bc991e460cdc481f89e251f15af9aa36371f70
TimMilne <tim.milne@target.com>
2019-03-10 23:04:56 -0500

    Capitalization bug

--- RFIDEncoderTests/TIAIEncoderTests.m
+++ RFIDEncoderTests/TIAIEncoderTests.m
@@ -10,32 +10,32 @@
 #import "TIAIEncoder.h"
 
 @interface TIAIEncoderTests : XCTestCase
-    {
-        TIAIEncoder *_encode;
-        NSString    *_asset_ref;
-        NSString    *_asset_id;
-        NSString    *_test_tiai_bin;
-        NSString    *_test_tiai_hex;
-        NSString    *_test_tiai_uri;
-        NSString    *_test_empty;
-    }
-    
-    @end
+{
+    TIAIEncoder *_encode;
+    NSString    *_asset_ref;
+    NSString    *_asset_id;
+    NSString    *_test_tiai_bin;
+    NSString    *_test_tiai_hex;
+    NSString    *_test_tiai_uri;
+    NSString    *_test_empty;
+}
+
+@end
 
 @implementation TIAIEncoderTests
-    
+
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
     if (_encode == nil) _encode = [TIAIEncoder alloc];
     _test_empty = @"";
 }
-    
+
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
-    
+
 - (void)testWithTiaiInt{
     _asset_ref = @"5";
     _asset_id  = @"1";
@@ -76,7 +76,7 @@
     XCTAssertEqualObjects(_test_empty    , [_encode asset_id_char], @"tiaiWithAssetIDDec: Test 3 Part 4 Failed");
     XCTAssertTrue([[_encode tiai_bin] length] == 96,                @"tiaiWithAssetIDDec: Test 3 Part 5 Failed");
 }
-    
+
 - (void)testWithTiaiChar{
     _asset_ref = @"7";
     _asset_id  = @"Y2A630";
@@ -103,8 +103,21 @@
     XCTAssertEqualObjects(_test_tiai_uri , [_encode tiai_uri],     @"tiaiWithAssetIDChar: Test 2 Part 3 Failed");
     XCTAssertEqualObjects(_test_empty    , [_encode asset_id_dec], @"tiaiWithAssetIDChar: Test 2 Part 4 Failed");
     XCTAssertTrue([[_encode tiai_bin] length] == 96,               @"tiaiWithAssetIDChar: Test 2 Part 5 Failed");
-}
     
+    _asset_ref = @"7";
+    _asset_id  = @"y2a630";
+    [_encode withAssetRef:_asset_ref assetIDChar:_asset_id];
+    _test_tiai_bin = @"000010100000000000000111000000000000000000000000000000000000011001110010000001110110110011110000";
+    _test_tiai_hex = @"0A0007000000000672076CF0";
+    _test_tiai_uri = @"urn:epc:tag:tiai-a-96:0.7.Y2A630";
+    
+    XCTAssertEqualObjects(_test_tiai_bin , [_encode tiai_bin],     @"tiaiWithAssetIDChar: Test 3 Part 1 Failed");
+    XCTAssertEqualObjects(_test_tiai_hex , [_encode tiai_hex],     @"tiaiWithAssetIDChar: Test 3 Part 2 Failed");
+    XCTAssertEqualObjects(_test_tiai_uri , [_encode tiai_uri],     @"tiaiWithAssetIDChar: Test 3 Part 3 Failed");
+    XCTAssertEqualObjects(_test_empty    , [_encode asset_id_dec], @"tiaiWithAssetIDChar: Test 3 Part 4 Failed");
+    XCTAssertTrue([[_encode tiai_bin] length] == 96,               @"tiaiWithAssetIDChar: Test 3 Part 5 Failed");
+}
+
 - (void)testWithTiaiHex{
     _asset_ref = @"8";
     _asset_id  = @"1";
@@ -162,7 +175,7 @@
     XCTAssertEqualObjects(_test_empty    , [_encode asset_id_char], @"tiaiWithAssetIDChar: Test 4 Part 5 Failed");
     XCTAssertTrue([[_encode tiai_bin] length] == 96,                @"tiaiWithAssetIDChar: Test 4 Part 6 Failed");
 }
-    
+
 - (void)testChar2Bin2Char{
     NSString *test_input       = @"#-/01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     NSString *test_output      = [_encode Char2Bin:test_input];
@@ -172,12 +185,12 @@
     XCTAssertEqualObjects(test_input,  test_round_trip,  @"tiaiChar2Bin2Char: Test 1 Part 1 Failed");
     XCTAssertEqualObjects(test_output, test_round_trip2, @"tiaiChar2Bin2Char: Test 1 Part 2 Failed");
 }
-    
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
 }
-    
-    @end
+
+@end
